200795ac815999c74b814600db804f9cef6d5cfa
Sergio Esteban <sergio.calvo@udea.edu.co>
2024-12-19 18:39:01 -0500

    Actualización de nextUi y tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-const {nextui} = require("@nextui-org/react");
+import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default{
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -9,6 +9,29 @@ module.exports = {
   ],
   theme: {
     extend: {
+        borderRadius: {
+          'radius-34': '34px',
+        
+
+        },
+        fontFamily: {
+          OpenSans: ['"Open Sans"', 'sans-serif'],
+          calligraffitti: ['Calligraffitti', 'cursive'],
+        },
+        colors:{
+          background1: '#121212',
+          background2: ' #8E05C226',
+          purpleStart: '#8E05C2',
+          purpleEnd: '#43025C',
+          pinkButton: '#D50891',
+          gris: "#D0D5DD",
+          grisFondo: "#F2F2F2",
+          grisFondo2: "#1F1F1F",
+          grisBoton: "#2C2C2C",
+          text: 'rgba(242, 242, 242, 0.44)',
+          textInput: "#B0B0B0CC",
+          sombraPink: "#D50891"
+        },  
     
     },
   },
