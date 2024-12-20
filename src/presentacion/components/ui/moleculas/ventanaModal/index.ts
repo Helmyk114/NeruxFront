76148dab836be3b4eb963131d201bea54ececa0f
Sergio Esteban <sergio.calvo@udea.edu.co>
2024-12-19 19:04:35 -0500

    Refactorización de la ventana modal

--- src/presentacion/components/ui/moleculas/ventanaModal/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as BodyModal } from './bodyModal';
-export { default as ContentModal } from './contentModal';
-export { default as FooterModal } from './footerModal';
-export { default as HeaderModal } from './headerModal';
--- src/utils/popUps/success.tsx
+++ src/utils/popUps/success.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NotificationTemplate from "../../presentacion/components/ui/template/notificaciones/notificacion";
-import ImagenSucces from "../../images/Variant8.png";
+//import ImagenSucces from "../../images/Variant8.png";
 
 interface PopUpSuccessProps {
   isOpen: boolean;
@@ -10,15 +10,12 @@ interface PopUpSuccessProps {
 const PopUpSuccess: React.FC<PopUpSuccessProps> = ({ isOpen, onClose }) => {
   return (
     <>
-      <NotificationTemplate
+      <NotificationTemplate 
         isOpen={isOpen}
         onClose={onClose}
-        imageUrl={ImagenSucces}
-        title="¡Listo! 🎉"
-        mensaje1={`Tu contraseña ha sido restablecida correctamente. ${"\n"}Ahora puedes iniciar sesión con tu nueva contraseña.`}
-        mensaje2={`${"\n"}¡Gracias por tu paciencia!`}
-        texto="Iniciar Sesión"
-        ruta="/test"
+        header={"Header"}
+        body={"Body"}
+        footer={"footer"}
       />
     </>
   );
