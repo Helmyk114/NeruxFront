8ac282a4b93a55d5d5ddbdd67a91a669481ff92f
Sergio Esteban <sergio.calvo@udea.edu.co>
2024-12-22 19:02:24 -0500

    Configuración de axios

---
+++ src/infrastructure/http/HttpClient.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosInstance } from "axios";
+
+export class HttpClient {
+  private axiosInstance: AxiosInstance;
+
+  constructor(baseUrl: string) {
+    this.axiosInstance = axios.create({
+      baseURL: baseUrl,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+  }
+
+  //Metods http basicos
+  public async get<T>(url: string): Promise<T> {
+    const respuesta = await this.axiosInstance.get(url);
+    return respuesta.data;
+  }
+
+  public async post<T, D>(url: string, data: D): Promise<T> {
+    const resultado = await this.axiosInstance.post(url, data);
+    return resultado.data;
+  }
+
+  public async put<T, D>(url: string, data: D): Promise<T> {
+    const respuesta = await this.axiosInstance.put(url, data);
+    return respuesta.data;
+  }
+
+  public async delete<T>(url: string): Promise<T> {
+    const respuesta = await this.axiosInstance.delete(url);
+    return respuesta.data;
+  }
+}
