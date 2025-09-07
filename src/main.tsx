1883b3f5bebdf996c5be3b70fd0909588fa3b134
Sergio Esteban <sergio.calvo@udea.edu.co>
2025-09-07 14:38:11 -0500

    Actualizar importación de estilos de Heroui a la nueva ruta de tema

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { HeroUIProvider, ToastProvider } from "@heroui/react";
 import { AppRouter } from "./routes/AppRoutes";
 import { setupInterceptors } from "./infrastructure";
-import "@heroui/react/styles.css"; 
+import "@heroui/theme/styles.css";
 import "./index.css";
 
 setupInterceptors();
