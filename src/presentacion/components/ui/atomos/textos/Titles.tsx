eeaaa46842e0a6b0b1bdd5dd1081810ffa3db0d7
Nathan <nathanndraws@gmail.com>
2025-09-29 18:36:36 -0500

    Se actualizo props de clase en componentes de título: Se hizo className opcional y se mejoro estilos

--- src/presentacion/components/ui/atomos/textos/Titles.tsx
+++ src/presentacion/components/ui/atomos/textos/Titles.tsx
@@ -1,22 +1,33 @@
 interface TitleProps {
   titulo: string;
-  className: string;
+  className?: string;
 }
 
+/*
+ * Configuración de componentes de título reutilizables con estilos predefinidos.
+ * 
+ * - PageTitle: Títulos grandes (24px, bold). Color: typography.first.
+ * - Subtitle: Subtítulos (16px, regular). Color: typography.second.
+ * - DrawerTitle: Títulos para Drawers o Modales (20px, semibold). Color: typography.first.
+ * - TableHeader: Encabezados para tablas (16px, bold). Color: typography.first.
+ * - InputTitle: Etiquetas para campos de formulario (14px, semibold). Color: typography.first.
+ * 
+*/
+
 export const Title = {
-  T1: ({ titulo, className }: TitleProps) => {
-    return <h1 className={className}>{titulo}</h1>;
+  PageTitle: ({ titulo, className }: TitleProps) => {
+    return <h1 className={`font-OpenSans text-2xl font-bold text-typography-first capitalize ${className || ''}`}>{titulo}</h1>;
   },
-  T2: ({ titulo, className }: TitleProps) => {
-    return <h2 className={className}>{titulo}</h2>;
+  Subtitle: ({ titulo, className }: TitleProps) => {
+    return <h2 className={`font-OpenSans text-base font-normal text-typography-second capitalize ${className || ''}`}>{titulo}</h2>;
   },
-  T3: ({ titulo, className }: TitleProps) => {
-    return <h3 className={className}>{titulo}</h3>;
+  DrawerTitle: ({ titulo, className }: TitleProps) => {
+    return <h3 className={`font-OpenSans text-xl font-semibold text-typography-first capitalize ${className || ''}`}>{titulo}</h3>;
   },
-  T4: ({ titulo, className }: TitleProps) => {
-    return <h4 className={className}>{titulo}</h4>;
+  TableHeader: ({ titulo, className }: TitleProps) => {
+    return <h4 className={`font-OpenSans text-base font-bold text-typography-first capitalize ${className || ''}`}>{titulo}</h4>;
   },
-  T5: ({ titulo, className }: TitleProps) => {
-    return <h5 className={className}>{titulo}</h5>;
+  InputTitle: ({ titulo, className }: TitleProps) => {
+    return <h5 className={`font-OpenSans text-sm font-semibold text-typography-first capitalize ${className || ''}`}>{titulo}</h5>;
   },
 };
